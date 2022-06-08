9e8fbb45ff635b294a7b95c9d947c643af09d8d2
Ashiheart <extrmediamond@yahoo.co.jp>
2022-06-09 08:54:25 +0900

    fix export_graph.c

--- export_graph.c
+++ export_graph.c
@@ -10,13 +10,11 @@ int main(void)
 
     task_make(stdin, &n, &task);
 
-    //task_export_graph(n, task, "graph.gv");
+    task_export_graph(n, task, "graph.gv");
 
     struct priority_list head = (struct priority_list) { .value = NULL, .next = NULL };
 
     plist_make(n, task, &head);
-
-    head->value = (struct priority_list*)malloc(sizeof(struct priority_list*)*n);
 
     plist_show(&head);
 
